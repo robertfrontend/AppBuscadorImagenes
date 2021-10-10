66f3bd01a560f3051e4bc15cbb985459be385af4
Robert Romero Medina <enanow41romero@gmail.com>
2021-10-10 12:13:27 -0400

    build push

---
+++ build/precache-manifest.4df6c30e8e60b66b0e8eae9716fed191.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92cb4217b336f350de8635e2701dc69d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8aa224948c09bc94dd5",
+    "url": "/static/css/main.aa1dc634.chunk.css"
+  },
+  {
+    "revision": "87cdeed4cdcfec416213",
+    "url": "/static/js/2.4445ce41.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4445ce41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8aa224948c09bc94dd5",
+    "url": "/static/js/main.c2a6e3da.chunk.js"
+  },
+  {
+    "revision": "fe423cd5e2c75492c3be",
+    "url": "/static/js/runtime-main.13109116.js"
+  },
+  {
+    "revision": "b47ad89d3fb10b5b702bab93526660bf",
+    "url": "/static/media/comments.b47ad89d.svg"
+  },
+  {
+    "revision": "9d0356f606ef6208077e638924331d5c",
+    "url": "/static/media/like.9d0356f6.svg"
+  }
+]);
